34848f4b6a8347fc8880fc17deae5fe02277c383
Yejin <yejin@example.com>
2025-11-19 12:48:21 +0900

    13주차

--- react2/src/app/blog/page.tsx
+++ react2/src/app/blog/page.tsx
@@ -1,8 +1,17 @@
 //import Posts from "@/components/posts";
-import {Suspense} from "react";
+// import {Suspense} from "react";
 
-export default function RootPage(){
-  <div>
-    <h1>Hello Bar!</h1>
-  </div>
+// export default function RootPage(){
+//   <div>
+//     <h1>Hello Bar!</h1>
+//   </div>
+// }
+
+import styles from "./blog.module.css";
+
+export default function BlogPage() {
+    <div>
+      <div className={styles.blog}>
+      </div>
+    </div>
 }
