35fdfe1cc0d6999c29866954609556df2a5a6dc8
Yejin <yejin@example.com>
2025-11-26 12:45:02 +0900

    14주차

--- react2/src/app/blog/page.tsx
+++ react2/src/app/blog/page.tsx
@@ -8,10 +8,18 @@
 // }
 
 import styles from "./blog.module.css";
+import Image from "next/image";
 
 export default function BlogPage() {
     <div>
       <div className={styles.blog}>
       </div>
+      <Image
+        className="dark:invert"
+        src="/next.js.png"
+        alt="Picture of the author"
+        width={600}
+        height={400}
+      />
     </div>
 }
